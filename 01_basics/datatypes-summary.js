332583f76cc025fe47e9feac56f377b504932a89
SATISH KUMAR CHOUDHARY <68549706+SATISHCHOUDHARY1998@users.noreply.github.com>
2024-06-23 12:45:30 +0000

    datatype

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,11 @@
+//# Primitive- Call by valuses
+
+//7 types : String, Number , boolearn, null, undefined, symbol, BigInt
+
+
+//# Refrence Type - (Non- Primitive)
+
+//Array, Objects, Functions
+
+
+ 
