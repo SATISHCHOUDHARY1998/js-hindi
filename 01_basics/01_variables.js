632180fbafc6ed8a9ab158bf760e3ed8aa753746
SATISH KUMAR CHOUDHARY <68549706+SATISHCHOUDHARY1998@users.noreply.github.com>
2024-06-23 04:06:38 +0000

    building

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "satish@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+//accountId = 72
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
