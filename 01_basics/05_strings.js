c0468590ade222a4595d74fe538d9cc1f58e6abb
SATISH KUMAR CHOUDHARY <68549706+SATISHCHOUDHARY1998@users.noreply.github.com>
2024-06-30 11:56:25 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name ="Satish"
+const repoCount = 50
+
+//console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
+
+const gameName = new String('Satish')
